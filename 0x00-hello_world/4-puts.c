473e9bbc45b232dcd53eccb7fc696c9cef67e20c
Amano Kelechi Philip <amanokelechi@gmail.com>
2022-09-10 00:40:59 -0700

    add

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,4 @@
- #include <stdio.h>
+#include <stdio.h>
 /**
  *  * main - main block
  *   * Return: 0
@@ -8,3 +8,4 @@ int main(void)
 		puts("\"Programming is like building a multilingual puzzle");
 			return (0);
 }
+
