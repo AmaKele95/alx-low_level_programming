04101ab06f37860f467e3487e392eb91669b2bf5
Amano Kelechi Philip <amanokelechi@gmail.com>
2022-09-13 20:29:24 -0700

    add

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,7 +2,7 @@
 
 
 /**
- *main -  prints _putchar
+ *main - prints _putchar
  *
  *Return: always 0 (Success)
  */
@@ -10,15 +10,14 @@
 int main(void)
 
 {
-	_putchar('H');
-	_putchar('o');
-	_putchar('l');
-	_putchar('b');
-	_putchar('e');
-	_putchar('r');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
 	_putchar('t');
-	_putchar('o');
-	_putchar('n');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 
 	return (0);
