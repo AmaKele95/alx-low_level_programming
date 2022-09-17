e4851ed21493becf9bf1021ac700211d2b7d4deb
Amano Kelechi Philip <amanokelechi@gmail.com>
2022-09-16 17:48:31 -0700

    add

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,71 +1,42 @@
 #include "main.h"
-
 #include <stdio.h>
 
 
 
 /**
+ *
+ * print_triangle - prints a triangle, followed by a new line
+ *
+ * @size: size of the triangle
+ *    */
 
- * main - prints the numbers from 1 to 100, followed by a new line
-
- * but for multiples of three prints Fizz instead of the number
-
- * and for the multiples of five prints Buzz
-
- * Return: Always 0 (Success)
-
- */
-
-int main(void)
+void print_triangle(int size)
 
 {
-
-	int i;
-
-
-
-	for (i = 1; i <= 100; i++)
-
+	if (size <= 0)
 	{
-
-		if (i % 3 == 0 && i % 5 != 0)
-
-		{
-
-			printf(" Fizz");
-
-		} else if (i % 5 == 0 && i % 3 != 0)
-
-		{
-
-			printf(" Buzz");
-
-		} else if (i % 3 == 0 && i % 5 == 0)
-
-		{
-
-			printf(" FizzBuzz");
-
-		} else if (i == 1)
-
-		{
-
-			printf("%d", i);
-
-		} else
-
-		{
-
-			printf(" %d", i);
-
-		}
-
+		_putchar('\n');
 	}
-
-	printf("\n");
-
-
-
-	return (0);
-
+       
+	else
+	{
+		int i, j;
+		
+		
+		for (i = 1; i <= size; i++)
+		{
+			
+			for (j = i; j < size; j++)
+			{
+				_putchar(' ');
+			}
+			
+			for (j = 1; j <= i; j++)
+			{
+				_putchar('#');
+			}
+			
+			_putchar('\n');
+		}
+	}
 }
