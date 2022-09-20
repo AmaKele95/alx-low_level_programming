265b281edcb683076d79cdddd9b75d89ce4d0b36
Amano Kelechi Philip <amanokelechi@gmail.com>
2022-09-20 06:07:56 -0700

    add

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -2,16 +2,15 @@
 #include "2-strlen.c"
 
 /**
- *
- *  * puts2 - puts string twice
+ * puts2 - puts2 string twice
  *
  * @str: string to put twice
  */
 void puts2(char *str)
 {
 	int i;
-	
-	
+
+
 	for (i = 0; i < _strlen(str); i++)
 	{
 		if (i % 2 == 0)
