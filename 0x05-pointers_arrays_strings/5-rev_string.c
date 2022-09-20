3c1d15847895631470b46f56c76792023d5658b4
Amano Kelechi Philip <amanokelechi@gmail.com>
2022-09-20 06:10:15 -0700

    add

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -2,7 +2,6 @@
 #include "2-strlen.c"
 
 /**
- *
  * rev_string - reverse a string value
  *
  * @s: string to print
@@ -11,8 +10,8 @@ void rev_string(char *s)
 {
 	int i;
 	char t;
-	
-	
+
+
 	for (i = 0; i < _strlen(s) / 2; i++)
 	{
 		t = s[i];
